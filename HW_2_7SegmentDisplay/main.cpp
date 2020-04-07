fa537fbbd5a3a45d3125af979c294e7a0a3c0023
Hua-Shan Wu <greenballow@gmail.com>
2020-04-08 02:31:33 +0800

    M

--- HW_2_7SegmentDisplay/main.cpp
+++ HW_2_7SegmentDisplay/main.cpp
@@ -3,10 +3,10 @@
 Serial pc( USBTX, USBRX );
 AnalogOut Aout(DAC0_OUT);
 AnalogIn Ain(A0);
-int sample = 1024;
+int sample = 128;
 int i;
-float ADCdata[1024];
-//for frequency calculate
+float ADCdata[128];
+//for frequency calculation
 int TimeState =0;
 Timer InitialTime;
 Timer FinalTime;
@@ -14,13 +14,14 @@ float initial = 0.0;
 float final = 0.0;
 float FrequencySum = 0.0;
 int ZeroNum = 0;
-int frequency = 0;
+int frequency;
 
 
 // 7 segment display setup( suppose frequency < 999 )
 BusOut display(D6, D7, D9, D10, D11, D5, D4, D8);
 char table[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
-char table1[3] = {0x3F,0x3F,0xBF};
+char table_2[10] = {0xBF, 0x86, 0xDB, 0xCF, 0xE6, 0xED, 0x87,0xFF, 0xEF};
+char table_1[3];
 
 // LED switch setup
 DigitalIn Switch(SW3);
@@ -31,16 +32,17 @@ DigitalOut greenLED(LED2);
 Thread thread1;
 Thread thread2;
 Thread thread3;
+Thread thread4;
 
 void wave_thread(){
   while(true){
     for (i = 0; i < sample; i++){
-      Aout = Ain;
       ADCdata[i] = Ain;
       wait(1./sample);
     }
     for (i = 0; i < sample; i++){ // print to python file
-      //pc.printf("%1.3f\r\n", ADCdata[i]);
+      pc.printf("%1.3f\r\n", ADCdata[i]);
+      // Calculate frequency
       InitialTime.start();
       FinalTime.start();
       if (ADCdata[i] == 0.3 && TimeState == 0){
@@ -61,9 +63,8 @@ void wave_thread(){
       }
       wait_us(100000);
     }
-    frequency = 100;
-    // round(FrequencySum/ZeroNum);
-    pc.printf("%7.3f\r\n", frequency);
+    //frequency = 100;
+    frequency = round(FrequencySum/ZeroNum);
   }
 }
 
@@ -72,9 +73,10 @@ void table_thread(){
     int hun = floor(frequency/100);
     int ten = floor((frequency-hun*100)/10);
     int one = frequency-hun*100-ten*10;
-    table1[0] = table[hun];
-    table1[1] = table[ten];
-    table[2] = 0x80|table[one];
+    table_1[0] = table[hun];
+    table_1[1] = table[ten];
+    table_1[2] = table_2[one];
+    wait_us(100000);
   }
 }
 
@@ -84,8 +86,8 @@ void segment_thread(){
       greenLED = 1;
       redLED = 0;
       for (int j = 0; j<3; j = j+1){
-        display = table1[j];
-        wait_us(100000);
+        display = table_1[j];
+        wait_us(500000);
       }
     }
     else{
@@ -93,9 +95,16 @@ void segment_thread(){
       greenLED = 0;
       for (int j = 0; j<3; j = j+1){
         display = 0x00;
-        wait_us(100000);
+        wait_us(500000);
       }
     }
+  }
+}
+
+void output_thread(){
+  while(true){
+    Aout = Ain;
+    wait(1./sample);
   }
 }
 
@@ -104,8 +113,11 @@ int main(){
   greenLED = 1;
   //Calculate sine wave frequency
   thread1.start(wave_thread);
+  //frequency = 100;
   //Save frequency value as table
   thread2.start(table_thread);
   // 7 segment display
   thread3.start(segment_thread);
+  //output sine wave from K66F to picoscope
+  thread4.start(output_thread);
 }
