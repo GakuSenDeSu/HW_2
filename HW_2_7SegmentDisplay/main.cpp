e21496be3231515658adbdbb03902b3e07798dab
Hua-Shan Wu <greenballow@gmail.com>
2020-04-07 23:04:58 +0800

    M

--- HW_2_7SegmentDisplay/main.cpp
+++ HW_2_7SegmentDisplay/main.cpp
@@ -6,6 +6,7 @@ AnalogIn Ain(A0);
 int sample = 256;
 int i;
 float ADCdata[256];
+//for frequency calculate
 int TimeState =0;
 float InitialTime = 0.0;
 float FinalTime = 0.0;
@@ -16,7 +17,8 @@ int frequency = 0;
 
 // 7 segment display setup( suppose frequency < 999 )
 BusOut display(D6, D7, D9, D10, D11, D5, D4, D8);
-char table[3] = {0x3F, 0x3F, 0xBF};
+char table[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
+char table1[3]
 
 // LED switch setup
 InterruptIn Switch(SW3);
@@ -32,11 +34,15 @@ void wave_thread(){
     for (i = 0; i < sample; i++){
       Aout = Ain;
       ADCdata[i] = Ain;
-      if (Ain == 0 && TimeState == 0){
+      wait(1./sample);
+    }
+    for (i = 0; i < sample; i++){ // print to python file
+      pc.printf("%1.3f\r\n", ADCdata[i]);
+      if (ADCdata[i] == 0 && TimeState == 0){
         TimeState = 1;
         InitialTime = time(NULL);
       }
-      else if(Ain == 0 && TimeState == 1){
+      else if(ADCdata[i] == 0 && TimeState == 1){
         TimeState = 0;
         FinalTime = time(NULL);
       }
@@ -46,14 +52,9 @@ void wave_thread(){
         FinalTime = 0;
         ZeroNum++;
       }
-      wait(1./sample);
-    }
-    for (i = 0; i < sample; i++){ // print to python file
-      //pc.printf("%1.3f\r\n", ADCdata[i]);
-      wait(0.1);
+      wait(0.01);
     }
     frequency = round(FrequencySum/ZeroNum);
-    pc.printf("%1.3f\r\n", frequency);
   }
 }
 
@@ -61,103 +62,8 @@ void table_thread(){
   while(true){
     int hun = floor(frequency/100);
     int ten = floor((frequency-hun*100)/10);
-    int one = floor((frequency-hun*100-ten*10));
-    switch (hun){
-      case 0:
-        table[1]={0x3F};
-        break;
-      case 1:
-        table[1]={0x06};
-        break;
-      case 2:
-        table[1]={0x5B};
-        break;
-      case 3:
-        table[1]={0x4F};
-        break;
-      case 4:
-        table[1]={0x66};
-        break;
-      case 5:
-        table[1]={0x6D};
-        break;
-      case 6:
-        table[1]={0x7D};
-        break;
-      case 7:
-        table[1]={0x07};
-        break;
-      case 8:
-        table[1]={0x7F};
-        break;
-      case 9:
-        table[1]={0x6F};
-        break;
-    }
-    switch (ten){
-      case 0:
-        table[2]={0x3F};
-        break;
-      case 1:
-        table[2]={0x06};
-        break;
-      case 2:
-        table[2]={0x5B};
-        break;
-      case 3:
-        table[2]={0x4F};
-        break;
-      case 4:
-        table[2]={0x66};
-        break;
-      case 5:
-        table[2]={0x6D};
-        break;
-      case 6:
-        table[2]={0x7D};
-        break;
-      case 7:
-        table[2]={0x07};
-        break;
-      case 8:
-        table[2]={0x7F};
-        break;
-      case 9:
-        table[2]={0x6F};
-        break;
-    }
-    switch (one){
-      case 0:
-        table[3]={0xBF};
-        break;
-      case 1:
-        table[3]={0x86};
-        break;
-      case 2:
-        table[3]={0xDB};
-        break;
-      case 3:
-        table[3]={0xCF};
-        break;
-      case 4:
-        table[3]={0xE6};
-        break;
-      case 5:
-        table[3]={0xED};
-        break;
-      case 6:
-        table[3]={0xFD};
-        break;
-      case 7:
-        table[3]={0x87};
-        break;
-      case 8:
-        table[3]={0xFF};
-        break;
-      case 9:
-        table[3]={0xEF};
-        break;
-    }
+    int one = frequency-hun*100-ten*10;
+    table1[3]={table[hun-1], table[ten-1], table[one-1]};
   }
 }
 
@@ -175,7 +81,7 @@ int main(){
       greenLED = 1;
       redLED = 0;
       for (int j = 0; j<3; j = j+1){
-        display = table[j];
+        display = table1[j];
         wait(1);
       }
     }
